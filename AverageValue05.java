56461c47d09337432bc4a9c19af5432339c448e5
alyfazeha <alyfazeha@gmail.com>
2024-11-08 08:15:02 +0700

    first commit

---
+++ AverageValue05.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AverageValue05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j;
+        float nilai, totalNilai, rataNilai=0;
+
+        i=1;
+        while (i<=5) {
+            System.out.println("Input nilai mahasiswa ke " + i);
+                totalNilai=0;
+                for(j=1; j<=5; j++){
+                    System.out.print("Nilai ke-" + j + " = ");
+                    nilai = sc.nextInt();
+                    totalNilai+=nilai;
+                }
+                rataNilai=totalNilai/5;
+                i++;  
+                System.out.println("Rata rata nilai mahasiswa ke " + i + " adalah " + rataNilai);
+        }
+    }
+}
